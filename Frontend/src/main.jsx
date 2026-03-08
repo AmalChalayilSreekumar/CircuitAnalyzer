682db5c50671920c62e46b84869bac61ce9ea205
AmalcSreekumar <amalsree548@gmail.com>
2026-03-08 03:59:58 -0400

    Added Main Home Page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Auth0Provider
-      domain="dev-dppbhr3symbhip0o.us.auth0.com"
-      clientId="xsCXBXWGWszulwp3dOl9kpl4RqQoJAjs"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0Provider
+        domain="dev-dppbhr3symbhip0o.us.auth0.com"
+        clientId="xsCXBXWGWszulwp3dOl9kpl4RqQoJAjs"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </StrictMode>
 );
